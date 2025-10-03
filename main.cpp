3e00e09525b6850e7cc392422a80b6bdb307c9a3
bmaletskiy <bmaletskij@knu.ua>
2025-10-03 21:02:55 +0300

    Initial working main with multiple draws

--- main.cpp
+++ main.cpp
@@ -3,10 +3,24 @@
 
 int main()
 {
+	int n, numDraws;
+	std::cout << "Enter max number on dominoes: ";
+	std::cin >> n;
+	std::cout << "Enter number of draws: ";
+	std::cin >> numDraws;
+
+	std::vector<int> sizes(numDraws);
 	Dominoes dom;
 
-	int size = dom.dealDominoes();
-	dom.show_matrix();
+	for (int i = 0; i < numDraws; ++i) {
+		dom.reset();
+		sizes[i] = dom.dealDominoes();
+	}
 
+	std::cout << "Draw sizes: ";
+	for(auto s: sizes){
+		std::cout << s << " ";
+	}
+	std::cout << std::endl;
 	return 0;
 }
